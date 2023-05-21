53232d3394d345ab4fed3ef9cc58e2220745dc80
Mikalai Zarazaka <golmtz@gmail.com>
2023-05-21 21:21:01 +0300

    ref to react router 6+

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,55 @@
-import { Route, Routes } from 'react-router-dom'
+import {
+	createBrowserRouter,
+	Route,
+	RouterProvider,
+	Routes,
+} from 'react-router-dom'
 
 import { PATH } from './constants/path'
 import { Layout } from './features'
 import { Home, Page404, Services } from './pages'
 
 const App = () => {
-	return (
-		<div>
-			<Routes>
-				<Route path={'/'} element={<Layout />}>
-					<Route index element={<Home />} />
-					<Route path={PATH.SERVICES} element={<Services />} />
-					<Route path={PATH.CONTACT} element={<h1>Контакты</h1>} />
-					<Route path={PATH.ABOUT_COMPANY} element={<h1>О компании</h1>} />
-					<Route path={'*'} element={<Page404 />} />
-				</Route>
-			</Routes>
-		</div>
-	)
+	const router = createBrowserRouter([
+		{
+			path: '/',
+			element: <Layout />,
+			errorElement: <Page404 />,
+			children: [
+				{
+					path: '/',
+					element: <Home />,
+					// index: true,
+				},
+				{
+					path: PATH.SERVICES,
+					element: <Services />,
+				},
+				{
+					path: PATH.CONTACT,
+					element: <Layout />,
+				},
+				{
+					path: PATH.ABOUT_COMPANY,
+					element: <Layout />,
+				},
+			],
+		},
+	])
+	return <RouterProvider router={router} />
+	// (
+	// 	<div>
+	// 		<Routes>
+	// 			<Route path={'/'} element={<Layout />}>
+	// 				<Route index element={<Home />} />
+	// 				<Route path={PATH.SERVICES} element={<Services />} />
+	// 				<Route path={PATH.CONTACT} element={<h1>Контакты</h1>} />
+	// 				<Route path={PATH.ABOUT_COMPANY} element={<h1>О компании</h1>} />
+	// 				<Route path={'*'} element={<Page404 />} />
+	// 			</Route>
+	// 		</Routes>
+	// 	</div>
+	// )
 }
 
 export default App
